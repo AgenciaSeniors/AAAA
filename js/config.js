654c2f34ebb82f336f067afcee2e71967d84be78
AgenciaSeniors <seniors.contacto@gmail.com>
2025-12-25 18:11:48 -0500

    ok

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    SUPABASE_URL: 'https://mvtatdvpsjynvayhhksc.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_XtV2kYHISXME2K-STuHmdw_UUGTZyvS',
+    SUPABASE_URL: 'https://xwkmhpcombsauoozyidi.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_5iDJi-xK69y1DM0nFYjqlw_TaozemSt',
     URL_SCRIPT: 'https://script.google.com/macros/s/AKfycbzzXvv1KtxUpBZVNfkhkZ6rI4iQEfk8SXHOgHeAa4jdH6-lLfKE-wswfMXtfaoeVMJC/exec',
 };
 
